b0bb07972fb987b7ad54b33d63cd58f5d4602f0c
tklae <github@klaesener.net>
2015-05-07 11:02:15 +0200

    tklae Store definitions by id instead of shortcut

--- test/test-searchEngines.js
+++ test/test-searchEngines.js
@@ -5,13 +5,20 @@ exports["test that it returns null for undefined shortcut"] = function(assert) {
 }
 
 exports["test that a shortcut can be added"] = function(assert) {
-    searchEngines.addShortcut("NameOfTheBestSearchEngine", "myShortcut", "urlWithPlaceholder%q")
+    searchEngines.addShortcut("someId", "someName", "myShortcut", "urlWithPlaceholder%q")
     assert.equal(searchEngines.getUrlFor("myShortcut", "Filled"), "urlWithPlaceholderFilled");
 }
 
 exports["test that a shortcut can be added from string description"] = function(assert) {
-    searchEngines.addShortcutFromPreferenceString("name,someShortcut,urlWithPlaceholder%q")
+    searchEngines.addShortcutFromPreferenceString("someId", "name,someShortcut,urlWithPlaceholder%q")
     assert.equal(searchEngines.getUrlFor("someShortcut", "Filled"), "urlWithPlaceholderFilled");
+}
+
+exports["test that a shortcut can be modified from string description if the id is the same"] = function(assert) {
+    searchEngines.addShortcutFromPreferenceString("someId", "name,someShortcut,urlWithPlaceholder%q")
+    searchEngines.addShortcutFromPreferenceString("someId", "name,anotherShortcut,anotherUrlWithPlaceholder%q")
+    assert.equal(searchEngines.getUrlFor("someShortcut", "Filled"), null);
+    assert.equal(searchEngines.getUrlFor("anotherShortcut", "Filled"), "anotherUrlWithPlaceholderFilled");
 }
 
 require("sdk/test").run(exports);
